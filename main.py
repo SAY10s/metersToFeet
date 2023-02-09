3ec6dfaf3b93ea0cadc29b3a062052d049892d00
Nikodem Majzner <nikodemmajzner@gmail.com>
2023-02-09 16:01:06 +0100

    Working conversion to meters

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
 class Data:
-    def metersToFeet(self, m):
-        print(f"{m}m are {m * 3.2808}ft")
+    units = ["meters", "centimeters", "kilometers", "feet", "inches", "yards"]
 
-    def feetToMeters(self, ft):
-        print(f"{ft}ft are {ft * 0.3048}m")
+    def __init__(self):
+        self.inputUnit = 1
+        self.inputValue = 0
+        self.meters = 0
 
+    def choose_units(self):
+        self.inputUnit = int(input("1. Meters\n2. Centimetres\n3. Kilometers\n4. Feet\n5. Inches\n6. Yards\nInsert your unit: "))
 
-data = Data()
-data.metersToFeet(30)
-data.feetToMeters(30)
+    def input_value(self):
+        self.inputValue = float(input(f"Insert value in {self.units[self.inputUnit-1]}: "))
+
+    def to_meters(self):
+        if self.inputUnit == 1:
+            self.meters = self.inputValue
+        if self.inputUnit == 2:
+            self.meters = self.inputValue * 0.01
+        if self.inputUnit == 3:
+            self.meters = self.inputValue * 1000
+        if self.inputUnit == 4:
+            self.meters = self.inputValue * 0.3048
+        if self.inputUnit == 5:
+            self.meters = self.inputValue * 0.0254
+        if self.inputUnit == 6:
+            self.meters = self.inputValue * 0.9144
+
+    def count4ever(self):
+        while True:
+            self.choose_units()
+            self.input_value()
+            self.to_meters()
+            print(self.meters)
+
+calculator = Data()
+calculator.count4ever()
+
